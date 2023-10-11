ea2f7b7a3a333c44c50995860919530af013bcb0
tracer-ics2022 <tracer@njuics.org>
2023-10-11 10:41:58 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-8TA12ITF 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:41:58 up 22 min,  1 user,  load average: 0.00, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -204,7 +204,8 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-  printf("+++++++++test++++++++++++\n");
+  *success=true;
+  //printf("+++++++++test++++++++++++\n");
   return eval(0,nr_token-1);
 
   /* TODO: Insert codes to evaluate the expression. */
