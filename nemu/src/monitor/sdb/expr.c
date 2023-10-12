9dae8630d1d7feaef0faf8536c4e2f3752755370
tracer-ics2022 <tracer@njuics.org>
2023-10-12 15:43:40 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-8TA12ITF 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:43:40 up  3:57,  1 user,  load average: 0.11, 0.04, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -249,7 +249,8 @@ word_t eval(int p, int q) {
   }
   if (IS_DEBUG_EXPR)
     printf("当前st:%d,ed:%d,eval对象为:", p, q);
-  tokensPrint(p, q);
+  if (IS_DEBUG_EXPR)
+    tokensPrint(p, q);
   if (IS_DEBUG_EXPR)
     printf("返回情况：");
   if (p > q) {
