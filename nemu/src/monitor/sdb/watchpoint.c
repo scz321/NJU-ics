81bc1f0e580e12fda5ec6a06b34f0c45159a9cd3
tracer-ics2022 <tracer@njuics.org>
2023-10-13 22:50:32 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-8TA12ITF 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:50:32 up  6:57,  1 user,  load average: 0.01, 0.05, 0.02

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -74,10 +74,10 @@ void free_wp(WP *wp){
 
 
 void watchpoint_display(){
-  printf("NO.\tCondation\n");
+  printf("NO.\tCondition\tcurrentValue\n");
   WP* cur = head;
   while (cur){
-    printf("\e[1;36m%d\e[0m\t\e[0;32m%s\e[0m\n", cur->NO, cur->condition);
+    printf("\e[1;36m%d\e[0m\t\e[0;32m%s\e[0m\t%d\n", cur->NO, cur->condition,cur->preVal);
     cur = cur->next;
   }
 }
