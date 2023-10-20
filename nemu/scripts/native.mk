b435c0b6fc66a31714ef69240369b83a8706b16a
tracer-ics2022 <tracer@njuics.org>
2023-10-20 13:25:49 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-8TA12ITF 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:25:49 up  8:10,  1 user,  load average: 0.02, 0.03, 0.00

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -29,7 +29,7 @@ override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
 IMG ?=
-NEMU_EXEC := $(BINARY) $(ARGS) -b $(IMG)
+NEMU_EXEC := $(BINARY) $(ARGS)  $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
 
