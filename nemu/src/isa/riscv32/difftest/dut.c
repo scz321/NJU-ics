2841b0920718978a42989d92c88c8deb32aa5200
tracer-ics2022 <tracer@njuics.org>
2023-10-31 21:19:40 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-8TA12ITF 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:19:40 up  8:52,  1 user,  load average: 0.20, 0.07, 0.02

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,21 +17,27 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
-bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
+{
 
-for (int i = 0; i < 32; ++i){
-    if (ref_r->gpr[i] != cpu.gpr[i])
-      return false;
-  }
+	for (int i = 0; i < 32; ++i)
+	{
+		printf("cpu.gpr[i]:0x%08x\t,ref_r->gpr[i]:0x%08x\n", cpu.gpr[i], ref_r->gpr[i]);
 
-  if (ref_r->pc == pc){
-    return true;
-  }else {
-    Log("%x\t%x", ref_r->pc, pc);
-    return false;
-  }
+		if (ref_r->gpr[i] != cpu.gpr[i])
+			return false;
+	}
+
+	if (ref_r->pc == pc)
+	{
+		return true;
+	}
+	else
+	{
+		Log("%x\t%x", ref_r->pc, pc);
+		return false;
+	}
 }
-
 
 void isa_difftest_attach() {
 }
