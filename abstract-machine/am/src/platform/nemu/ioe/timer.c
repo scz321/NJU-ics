610e82b96f4fa9fc73162abbde68bff22823c0ca
tracer-ics2022 <tracer@njuics.org>
2023-11-02 21:40:23 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-8TA12ITF 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:40:23 up  5:02,  8 users,  load average: 0.09, 0.04, 0.01

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,11 +1,18 @@
 #include <am.h>
 #include <nemu.h>
+#include <sys/time.h>//新增，用来获取当前系统时间
 
+static struct timeval boot_time = {};
 void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+	 
+  struct timeval now;
+  gettimeofday(&now, NULL);
+  long seconds = now.tv_sec - boot_time.tv_sec;
+  long useconds = now.tv_usec - boot_time.tv_usec;
+  uptime->us = seconds * 1000000 + (useconds + 500);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
