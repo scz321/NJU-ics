567bae41de0ebf61e56b5c550a4ee3c5959e9d76
tracer-ics2022 <tracer@njuics.org>
2023-11-08 23:30:35 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-8TA12ITF 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:30:35 up 12:47,  1 user,  load average: 0.13, 0.09, 0.03

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,15 +1,46 @@
 #include <am.h>
 #include <nemu.h>
 
+#include <stdio.h>//这里按理说是不需要include的呀
+
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+// void __am_gpu_init()
+// {
+// 	int i;
+// 	int w = 400; // TODO: get the correct width
+// 	int h = 300; // TODO: get the correct height
+// 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;//这里会触发回调
+// 	for (i = 0; i < w * h; i++)
+// 		fb[i] = i;//这里本质上是对VGA的像素值进行随机写入
+// 	outl(SYNC_ADDR, 1);
+// }
+
+// void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+//   *cfg = (AM_GPU_CONFIG_T) {
+//     .present = true, .has_accel = false,
+//     .width = 0, .height = 0,
+//     .vmemsz = 0
+//   };
+// }
 void __am_gpu_init() {
+  int i;
+  int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
+  int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
+  printf("w:%d, h:%d\n", w, h);
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  //这里会触发一次回调函数
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t info = inl(VGACTL_ADDR);//这里也可能触发一次回调函数
+  uint16_t height = (uint16_t)(info & 0xFFFF);
+  uint16_t width = (uint16_t)(info >> 16);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = width, .height = height,
     .vmemsz = 0
   };
 }
