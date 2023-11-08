6bc8bc8b2c920292b5c5bf4718a0d80b7eee91f2
tracer-ics2022 <tracer@njuics.org>
2023-11-08 19:57:58 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-8TA12ITF 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:57:58 up  9:27,  1 user,  load average: 0.29, 0.15, 0.48

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -70,6 +70,7 @@ static uint32_t key_dequeue() {
 }
 
 void send_key(uint8_t scancode, bool is_keydown) {
+	printf("执行了一次send_key\n");
   if (nemu_state.state == NEMU_RUNNING && keymap[scancode] != _KEY_NONE) {
     uint32_t am_scancode = keymap[scancode] | (is_keydown ? KEYDOWN_MASK : 0);
     key_enqueue(am_scancode);
@@ -88,7 +89,7 @@ static uint32_t key_dequeue() {
 static uint32_t *i8042_data_port_base = NULL;
 
 static void i8042_data_io_handler(uint32_t offset, int len, bool is_write) {
-	printf("触发keyboard回调函数执行\n");
+	//printf("触发keyboard回调函数执行\n");
   assert(!is_write);
   assert(offset == 0);
   i8042_data_port_base[0] = key_dequeue();
