9a04bcd88eb614fe050a3c132e3957f38c14f9ea
tracer-ics2022 <tracer@njuics.org>
2023-11-09 11:28:23 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-8TA12ITF 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:28:23 up 16:40,  1 user,  load average: 0.25, 0.21, 0.19

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -74,7 +74,7 @@ static inline void update_screen() {
 
 void vga_update_screen()
 {
-	printf("执行一次update\n");
+	//printf("执行一次update\n");
 	// TODO: call `update_screen()` when the sync register is non-zero,
 	// then zero out the sync register
 	update_screen();
