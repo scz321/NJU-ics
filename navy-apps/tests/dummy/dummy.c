5f2052113bc2d25a6ea785bf6b400c3d6b6d0e40
tracer-ics2022 <tracer@njuics.org>
2024-01-14 11:40:49 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-8TA12ITF 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:40:49 up 46 min,  1 user,  load average: 0.06, 0.05, 0.01

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -7,18 +7,18 @@
 #define SYS_yield 1
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
-void mulprint(){
-	int i, j;
+// void mulprint(){
+// 	int i, j;
 
-    for (i = 1; i <= 9; i++) {
-        for (j = 1; j <= 9; j++) {
-            printf("%d x %d = %d\t", i, j, i * j);
-        }
-        printf("\n");
-    }
-}
+//     for (i = 1; i <= 9; i++) {
+//         for (j = 1; j <= 9; j++) {
+//             printf("%d x %d = %d\t", i, j, i * j);
+//         }
+//         printf("\n");
+//     }
+// }
 int main() {
 	
   return _syscall_(SYS_yield, 0, 0, 0);
-	mulprint();
+	//mulprint();
 }
