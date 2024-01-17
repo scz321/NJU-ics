e47fa7775ff7e2edff5576f4061e5f95bdf1d1ad
tracer-ics2022 <tracer@njuics.org>
2024-01-17 14:19:57 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-8TA12ITF 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:19:57 up 1 day,  2:02,  1 user,  load average: 0.11, 0.12, 0.09

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,7 +25,7 @@ void init_proc() {
   Log("Initializing processes...");
 	
 	//naive_uload(NULL,NULL);
-	naive_uload(NULL,"/bin/events-test");
+	naive_uload(NULL,"/bin/event-test");
   // load program here
 
 }
