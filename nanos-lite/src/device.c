d945f93963030c4725d1182dc8df5b78375032eb
tracer-ics2022 <tracer@njuics.org>
2024-01-17 14:29:17 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-8TA12ITF 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:29:17 up 1 day,  2:11,  1 user,  load average: 0.23, 0.18, 0.16

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,6 +28,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 
 //offset被忽视
 size_t events_read(void *buf, size_t offset, size_t len) {
+  printf("events_read正在被调用\n");
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE) return 0;
   
